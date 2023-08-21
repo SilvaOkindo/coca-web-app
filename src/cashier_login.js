1b0fc29ef8f9ad627de504429441c026fcb5c864
silvaokindo <okindosilva@gmail.com>
2023-08-21 09:36:37 +0300

    first commit

---
+++ src/cashier_login.js
@@ -0,0 +1,4 @@
+import '../styles/modern-normalizer.css'
+import '../styles/style.css'
+import '../styles/components/cashier_login.css'
+import '../styles/utils.css'
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import '../styles/modern-normalizer.css'
 import '../styles/style.css'
+import '../styles/components/login.css'
 import '../styles/utils.css'
